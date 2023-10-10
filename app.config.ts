905ecd57e5e2d757225d46db24ea045e88edeac8
lif3ng <lif3ng@icloud.com>
2023-10-10 10:34:42 +0800

    test change
    test desc

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,13 @@
 export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
+
   socials: {
     twitter: 'Atinux',
     github: 'Atinux/content-wind'
+  },
+
+  nuxtIcon: {
+    size: '2em'
   }
 })
